9186957c6acf76f70ba6db9ebe9b700d157ec7a5
Gabriel Sioson <siosongabriel2@gmail.com>
2026-03-14 20:10:46 +0800

    Created some classes

---
+++ MyLib/Agency.java
@@ -0,0 +1,18 @@
+package MyLib;
+
+import java.util.List;
+
+public class Agency {
+    private String companyName;
+    private List<Property> properties;
+    private List<Agent> agents;
+    private Report report;
+
+    Agency(String companyName) {
+
+    }
+
+    public void generateReport() {
+        
+    }
+}
