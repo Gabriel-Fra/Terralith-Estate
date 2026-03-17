5cd4505a2a7d10fed1de60cab10b457ff39c17c1
Gabriel Sioson <siosongabriel2@gmail.com>
2026-03-17 20:41:20 +0800

    Main

--- MyApp/Main.java
+++ MyApp/Main.java
@@ -1,15 +1,15 @@
 package MyApp;
 
+import MyGui.MainFrame;
 import javax.swing.SwingUtilities;
 
 
 public class Main {
     public static void main(String[] args) {
-        /*
         SwingUtilities.invokeLater(() ->
             {
                 new MainFrame().setVisible(true);
             }
-        ); */
+        ); 
     }
 }
