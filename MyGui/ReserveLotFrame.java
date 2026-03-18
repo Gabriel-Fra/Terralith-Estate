7e0c830977aa4c170ed92de17810774a7cc5eb38
Gabriel Sioson <siosongabriel2@gmail.com>
2026-03-18 18:36:49 +0800

    .

--- MyGui/ReserveLotFrame.java
+++ MyGui/ReserveLotFrame.java
@@ -34,24 +34,53 @@ public class ReserveLotFrame extends javax.swing.JFrame {
     }
 
     private void loadLots() {
+        cmbLot.removeActionListener(this::cmbLotActionPerformed);
         cmbLot.removeAllItems();
 
         if (cmbBlock.getSelectedItem() == null) {
+            cmbLot.addActionListener(this::cmbLotActionPerformed);
             return;
         }
 
         int blockNum = Integer.parseInt(((String) cmbBlock.getSelectedItem()).split(" ")[1]);
         Block b = Agency.getInstance().getMainProperty().getBlock(blockNum);
 
-        for (Lot l : b.getAvailableLots()) {
-            cmbLot.addItem("Lot " + l.getLotNumber()
-                    + " | " + l.getModelName()
-                    + " | PHP " + String.format("%,.2f", l.getTotalContractPrice()));
+        for (Lot l : b.getLots()) {
+            if (l.getStatus().equals(Lot.AVAILABLE)) {
+                cmbLot.addItem("Lot " + l.getLotNumber()
+                        + " | " + l.getModelName()
+                        + " | PHP " + String.format("%,.2f", l.getTotalContractPrice())
+                        + " [AVAILABLE]");
+            } else if (l.getStatus().equals(Lot.RESERVED)) {
+                cmbLot.addItem("Lot " + l.getLotNumber()
+                        + " | " + l.getModelName()
+                        + " | PHP " + String.format("%,.2f", l.getTotalContractPrice())
+                        + " [RESERVED by: " + l.getOwner().getName() + "]");
+            }
         }
 
         if (cmbLot.getItemCount() == 0) {
             cmbLot.addItem("No available lots");
         }
+
+        cmbLot.addActionListener(this::cmbLotActionPerformed);
+        updateButtons();
+    }
+
+    private void updateButtons() {
+        if (cmbLot.getSelectedItem() == null
+                || cmbLot.getSelectedItem().toString().startsWith("No")) {
+            reserve.setEnabled(false);
+            forfeit.setEnabled(false);
+            return;
+        }
+
+        String lotText = (String) cmbLot.getSelectedItem();
+        boolean isReserved = lotText.contains("[RESERVED");
+        boolean isAvailable = lotText.contains("[AVAILABLE]");
+
+        reserve.setEnabled(isAvailable);
+        forfeit.setEnabled(isReserved);
     }
 
     private void loadAgents() {
@@ -105,6 +134,47 @@ public class ReserveLotFrame extends javax.swing.JFrame {
         txtContact.setText("");
         loadLots();
     }
+
+    private void processForfeit() {
+        if (cmbLot.getSelectedItem() == null
+                || cmbLot.getSelectedItem().toString().startsWith("No")) {
+            return;
+        }
+
+        int blockNum = Integer.parseInt(((String) cmbBlock.getSelectedItem()).split(" ")[1]);
+        Block b = Agency.getInstance().getMainProperty().getBlock(blockNum);
+        String lotText = (String) cmbLot.getSelectedItem();
+        int lotNum = Integer.parseInt(lotText.split(" ")[1]);
+        Lot selectedLot = b.getLot(lotNum);
+
+        String ownerName = selectedLot.getOwner() != null ? selectedLot.getOwner().getName() : "Unknown";
+
+        int confirm = javax.swing.JOptionPane.showConfirmDialog(this,
+            "Are you sure you want to forfeit the reservation?\n\n"
+            + "Client      : " + ownerName + "\n"
+            + "Block " + blockNum + " - Lot " + lotNum + "\n"
+            + "House Model : " + selectedLot.getModelName() + "\n\n"
+            + "This will set the lot back to AVAILABLE.",
+            "Confirm Forfeit",
+            javax.swing.JOptionPane.YES_NO_OPTION,
+            javax.swing.JOptionPane.WARNING_MESSAGE);
+
+        if (confirm == javax.swing.JOptionPane.YES_OPTION) {
+            selectedLot.getReservation().forfeit();
+            selectedLot.setStatus(Lot.AVAILABLE);
+            selectedLot.updateOwner(null);
+            selectedLot.setReservation(null);
+
+            Log.record("FOR-" + lotNum, "FORFEITED - Lot " + lotNum
+                + " Block " + blockNum + " by " + ownerName);
+
+            javax.swing.JOptionPane.showMessageDialog(this,
+                "Reservation forfeited.\n"
+                + "Lot " + lotNum + " (Block " + blockNum + ") is now AVAILABLE.");
+
+            loadLots();
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -127,6 +197,7 @@ public class ReserveLotFrame extends javax.swing.JFrame {
         close = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
         txtContact = new javax.swing.JTextField();
+        forfeit = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -144,6 +215,7 @@ public class ReserveLotFrame extends javax.swing.JFrame {
         cmbBlock.addActionListener(this::cmbBlockActionPerformed);
 
         cmbLot.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmbLot.addActionListener(this::cmbLotActionPerformed);
 
         cmbAgent.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
@@ -161,36 +233,42 @@ public class ReserveLotFrame extends javax.swing.JFrame {
 
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/MyGui/Images/Logo.png"))); // NOI18N
 
+        forfeit.setText("Forfeit");
+        forfeit.addActionListener(this::forfeitActionPerformed);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(reserve)
+                        .addGap(18, 18, 18)
+                        .addComponent(forfeit)
+                        .addGap(18, 18, 18)
+                        .addComponent(close, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(52, 52, 52)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(38, 38, 38)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtName)
-                                    .addComponent(cmbBlock, 0, 231, Short.MAX_VALUE)
-                                    .addComponent(cmbLot, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(cmbAgent, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(txtContact)))
-                            .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 352, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(109, 109, 109)
-                        .addComponent(reserve)
-                        .addGap(48, 48, 48)
-                        .addComponent(close, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(57, Short.MAX_VALUE))
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(38, 38, 38)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtName)
+                            .addComponent(txtContact)
+                            .addComponent(cmbAgent, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(cmbLot, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cmbBlock, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addGap(139, 139, 139))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(111, 111, 111)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 352, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -217,11 +295,12 @@ public class ReserveLotFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(cmbAgent, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                .addGap(37, 37, 37)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(close)
                     .addComponent(reserve)
-                    .addComponent(close))
-                .addGap(38, 38, 38))
+                    .addComponent(forfeit))
+                .addContainerGap(52, Short.MAX_VALUE))
         );
 
         pack();
@@ -240,6 +319,16 @@ public class ReserveLotFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_closeActionPerformed
+
+    private void forfeitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_forfeitActionPerformed
+        // TODO add your handling code here:
+        processForfeit();
+    }//GEN-LAST:event_forfeitActionPerformed
+
+    private void cmbLotActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbLotActionPerformed
+        // TODO add your handling code here:
+        updateButtons();
+    }//GEN-LAST:event_cmbLotActionPerformed
 
     /**
      * @param args the command line arguments
@@ -271,6 +360,7 @@ public class ReserveLotFrame extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> cmbAgent;
     private javax.swing.JComboBox<String> cmbBlock;
     private javax.swing.JComboBox<String> cmbLot;
+    private javax.swing.JButton forfeit;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
